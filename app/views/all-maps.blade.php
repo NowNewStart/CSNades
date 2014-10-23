0167e0514189e6294a33665c43b8c34063b70cf4
James LaChance <fatboyxpc@gmail.com>
2014-10-23 12:26:55 -0400

    Centered the map names under the images

--- app/views/all-maps.blade.php
+++ app/views/all-maps.blade.php
@@ -3,7 +3,7 @@
 @section('content')
         <div class="row">
         @foreach($maps as $map)
-            <div class="col-lg-3">
+            <div class="col-lg-3 text-center">
                 <a href="{{ action('MapsController@showMap', array('slug' => $map->slug)) }}">
                     <img src="{{ $map->image }}" alt="{{ $map->name }}" class="img-responsive hidden-xs hidden-sm"/>
                     <h3>{{ $map->name }}</h3>
