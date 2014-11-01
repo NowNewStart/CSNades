cbbe2ab9011952d0fc4ab146164a8b8de8912400
Jung Oh <Jung3o@yahoo.com>
2014-10-31 23:47:12 -0500

    yay nades

--- app/views/nades/nade-form.blade.php
+++ app/views/nades/nade-form.blade.php
@@ -1,18 +1,20 @@
 @extends('layouts.main')
 
 @section('content')
-        <div class="row">
-            {{ Form::open(array('method' => 'post', 'action' => 'NadesController@saveNade', 'class' => 'form-horizontal')) }}
-                <div class="col-xs-12 col-sm-4">
+        {{ Form::open(array('method' => 'post', 'action' => 'NadesController@saveNade', 'class' => 'form-horizontal')) }}
+            <div class="row">
+                <div class="col-xs-12 col-sm-4 col-sm-offset-2">
                     <div class="form-group">
                         <label for="title" class="col-sm-12">Nade Title</label>
-                        <div class="col-sm-8">
+                        <div class="col-sm-12">
                             <input type="text" name="title" id="title" placeholder="Title" class="form-control">
                         </div>
                     </div>
+                </div>
+                <div class="col-xs-12 col-sm-4">
                     <div class="form-group">
                         <label for="map" class="col-sm-12">Map</label>
-                        <div class="col-sm-8">
+                        <div class="col-sm-12">
                             <select name="map" id="map" class="form-control">
                                 @foreach ($maps as $map)
                                 <option value="{{ $map->id }}">{{ $map->name }}</option>
@@ -20,9 +22,13 @@
                             </select>
                         </div>
                     </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-4 col-sm-offset-2">
                     <div class="form-group">
                         <label for="pop-spot" class="col-sm-12">Pop Spot</label>
-                        <div class="col-sm-8">
+                        <div class="col-sm-12">
                             <select name="pop_spot" id="pop-spot" class="form-control">
                                 @foreach ($popSpots as $popSpot => $popName)
                                 <option value="{{ $popSpot }}">{{ $popName }}</option>
@@ -32,10 +38,39 @@
                     </div>
                     <div class="form-group">
                         <label for="imgur-album" class="col-sm-12">Imgur Link</label>
-                        <div class="col-sm-8">
+                        <div class="col-sm-12">
                             <input type="text" name="imgur_album" id="imgur-album" placeholder="http://imgur.com/a/album" class="form-control">
                         </div>
                     </div>
+                    <div class="form-group">
+                        <label for="youtube" class="col-sm-12">YouTube Link</label>
+                        <div class="col-sm-12">
+                            <input type="text" name="youtube" id="youtube" placeholder="https://www.youtube.com/watch?v=video" class="form-control">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-4">
+                    <div class="form-group">
+                        <label class="col-sm-12">Nade Type</label>
+                        <div class="col-xs-12">
+                            @foreach (Nade::getNadeTypes() as $nadeTypeKey => $nadeType)
+                            <div class="radio">
+                                <label>
+                                    <input type="radio" name="type" value="{{ $nadeTypeKey }}">
+                                        <i class="{{ $nadeType['class'] }}" title="{{ $nadeType['label'] }}"></i> {{ $nadeType['label'] }}
+                                </label>
+                            </div>
+                            @endforeach
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="tags" class="col-sm-12">Tags</label>
+                        <div class="col-sm-12">
+                            <textarea rows="2" name="tags" id="tags" placeholder="double stack, car, garage" class="form-control"></textarea>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-4 col-sm-offset-2">
                     <div class="form-group">
                         <div class="col-xs-12">
                             <div class="checkbox">
@@ -56,41 +91,12 @@
                         </div>
                         @endif
                     </div>
-                </div>
-                <div class="col-xs-12 col-sm-4">
-                    <div class="form-group">
-                        <label class="col-sm-12">Nade Type</label>
-                        <div class="col-xs-12">
-                            @foreach (Nade::getNadeTypes() as $nadeTypeKey => $nadeType)
-                            <div class="radio">
-                                <label>
-                                    <input type="radio" name="type" value="{{ $nadeTypeKey }}">
-                                        <i class="{{ $nadeType['class'] }}" title="{{ $nadeType['label'] }}"></i> {{ $nadeType['label'] }}
-                                </label>
-                            </div>
-                            @endforeach
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="youtube" class="col-sm-12">YouTube Link</label>
-                        <div class="col-sm-8">
-                            <input type="text" name="youtube" id="youtube" placeholder="https://www.youtube.com/watch?v=video" class="form-control">
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="tags" class="col-sm-12">Tags</label>
-                        <div class="col-sm-8">
-                            <input type="text" name="tags" id="tags" placeholder="double stack, car, garage" class="form-control">
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xs-12">
                     <div class="form-group">
                         <div class="col-xs-12">
                             <button type="submit" class="btn btn-primary">Submit Nade</button>
                         </div>
                     </div>
                 </div>
-            {{ Form::close() }}
-        </div>
+            </div>
+        {{ Form::close() }}
 @stop
