98301704bd8d463a111237242e50c77b94d7b294
James LaChance <fatboyxpc@gmail.com>
2014-11-02 05:21:55 -0500

    Changed the p tag to span for form errors.

--- app/views/users/add-user.blade.php
+++ app/views/users/add-user.blade.php
@@ -8,28 +8,28 @@
               <label for="email" class="col-xs-12">Email</label>
               <div class="col-xs-12">
                 <input type="text" name="email" id="email" class="form-control" value="{{ Input::old('email') }}">
-                {{ $errors->first('email', '<p>:message</p>') }}
+                {{ $errors->first('email', '<span>:message</span>') }}
               </div>
             </div>
             <div class="form-group {{ $errors->has('username') ? 'has-error' : 'has-success' }}">
               <label for="username" class="col-xs-12">Username</label>
               <div class="col-xs-12">
                 <input type="text" name="username" id="username" class="form-control" placeholder="At least 6 characters" value="{{ Input::old('username') }}">
-                {{ $errors->first('username', '<p>:message</p>') }}
+                {{ $errors->first('username', '<span>:message</span>') }}
               </div>
             </div>
             <div class="form-group {{ $errors->has('password') ? 'has-error' : 'has-success' }}">
               <label for="password" class="col-xs-12">Password</label>
               <div class="col-xs-12">
                 <input type="password" name="password" id="password" class="form-control" placeholder="At least 8 characters">
-                {{ $errors->first('password', '<p>:message</p>') }}
+                {{ $errors->first('password', '<span>:message</span>') }}
               </div>
             </div>
             <div class="form-group {{ $errors->has('password2') ? 'has-error' : 'has-success' }}">
               <label for="password2" class="col-xs-12">Confirm Password</label>
               <div class="col-xs-12">
                 <input type="password" name="password2" id="password2" class="form-control">
-                {{ $errors->first('password2', '<p>:message</p>') }}
+                {{ $errors->first('password2', '<span>:message</span>') }}
               </div>
             </div>
             <div class="form-group">
