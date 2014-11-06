9e34aa779db7f6a5f6485b11015d48260a774f07
Jung Oh <Jung3o@yahoo.com>
2014-11-05 22:20:33 -0600

    #11 fixed some feature view

--- app/views/home/features.blade.php
+++ app/views/home/features.blade.php
@@ -3,8 +3,8 @@
 @section('content')
     <div class="features row">
         @foreach($features as $feature)
-        <div class="item col-xs-6">
-            <div>
+        <div class="col-xs-6">
+            <div class="item">
                 <h4>{{ $feature['title'] }} &mdash; <small>{{{ date("F j, Y", strtotime($feature['created_at'])) }}}</small></h4>
                 <p>{{ $feature['description'] }}</p>
             </div>
