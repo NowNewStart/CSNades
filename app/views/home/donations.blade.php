04327f51934fc2885ddb9647dd6f7ee06ed8512b
James LaChance <fatboyxpc@gmail.com>
2014-11-08 18:32:43 -0500

    Changed the donation form

--- app/views/home/donations.blade.php
+++ app/views/home/donations.blade.php
@@ -4,10 +4,20 @@
     <div class="row">
         <div class="col-xs-12">
             <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-                <input type="hidden" name="cmd" value="_s-xclick">
-                <input type="hidden" name="hosted_button_id" value="L576T2XU3DYLG">
-                <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-                <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+                <input type="hidden" name="cmd" value="_donations">
+                <input type="hidden" name="item_name" value="Donation">
+                <input type="hidden" name="business" value="csnades+paypal@gmail.com">
+                <input type="hidden" name="return" value="{{ urlencode(route('get.donations')) }}">
+                <input type="hidden" name="cancel_return" value="{{ urlencode(route('get.donations')) }}">
+                <input type="hidden" name="cbt" value="Go Back To CSNades">
+                <input type="hidden" name="rm" value="1">
+                <input type="hidden" name="no_note" value="1">
+                <input type="hidden" name="no_shipping" value="1">
+                <input type="hidden" name="lc" value="US">
+                <input type="hidden" name="currency_code" value="USD">
+                <input type="hidden" name="bn" value="PP-DonationsBF:btn_donate_LG.gif:NonHostedGuest">
+                <input type="image" src="https://www.paypal.com/en_US/i/btn/btn_donate_LG.gif" name="submit" alt="PayPal - The safer, easier way to pay online!">
+                <!-- <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"> -->
             </form>
         </div>
     </div>
